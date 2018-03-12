d5e2410dd1d4f905957e90959569cb1f01c115ec
pyrop2000 <pyrop2000@gmail.com>
2018-03-12 14:16:14 +0000

    download data

---
+++ scripts/downRead.R
@@ -0,0 +1,24 @@
+down_self <- function(url, folder) {
+# Download file without specifying name
+      urlsplit = strsplit(url, "/")[[1]]
+      filename = urlsplit[length(urlsplit)]
+      path = paste(folder, "/", filename, sep = "")      
+      download.file(url, destfile = path)
+}
+
+
+# raw data for the project
+url_rd = "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
+download.file(url_rd, destfile = "data/rawdata.zip")
+
+
+# paper on Journal of Statistical Software on text mining infrastructure in R
+jss = NULL
+jss[1] <- "https://www.jstatsoft.org/index.php/jss/article/view/v025i05/v25i05.pdf"
+jss[2] <- "https://www.jstatsoft.org/index.php/jss/article/downloadSuppFile/v025i05/tm_0.3.tar.gz"
+jss[3] <- "https://www.jstatsoft.org/index.php/jss/article/downloadSuppFile/v025i05/v25i05.R"
+
+invisible(lapply(jss, down_self, folder = "knowledge"))
+
+# clear workspace
+rm(list = ls())
