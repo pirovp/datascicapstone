69a764faafb13480a2f4f8666016fb840dfd7df1
pyrop2000 <pyrop2000@gmail.com>
2018-03-18 13:57:22 +0000

    download data

--- scripts/downRead.R
+++ scripts/downRead.R
@@ -10,13 +10,14 @@ down_self <- function(url, folder) {
 # raw data for the project
 url_rd = "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
 download.file(url_rd, destfile = "data/rawdata.zip")
+unzip("data/rawdata.zip", exdir = "data")
 
 
 # paper on Journal of Statistical Software on text mining infrastructure in R
 jss = NULL
 jss[1] <- "https://www.jstatsoft.org/index.php/jss/article/view/v025i05/v25i05.pdf"
-jss[2] <- "https://www.jstatsoft.org/index.php/jss/article/downloadSuppFile/v025i05/tm_0.3.tar.gz"
-jss[3] <- "https://www.jstatsoft.org/index.php/jss/article/downloadSuppFile/v025i05/v25i05.R"
+# jss[2] <- "https://www.jstatsoft.org/index.php/jss/article/downloadSuppFile/v025i05/twww.mbeglobal.com m_0.3.tar.gz"
+jss[2] <- "https://www.jstatsoft.org/index.php/jss/article/downloadSuppFile/v025i05/v25i05.R"
 
 invisible(lapply(jss, down_self, folder = "knowledge"))
 
