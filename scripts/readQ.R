fd230821c31fc8bb944cb4bb2eaacec146c13761
pyrop2000 <pyrop2000@gmail.com>
2018-11-11 23:02:40 +0000

    -

--- scripts/readQ.R
+++ scripts/readQ.R
@@ -7,20 +7,10 @@ library(quanteda)
 #save(encorp, file = "data/corpusQ.Rdata")
 
 # With data from read.R instead:
-
-load("data/fullcorpora.Rdata")
-#load("data/smallsample.Rdata") # small sample corpus to try a few things quickly
+#load("data/fullcorpora.Rdata")
+load("data/smallsample.Rdata") # small sample corpus to try a few things quickly
 cblogs <- corpus(blogs, metacorpus = list(source = "blogs"))
 cnews <-  corpus(news, metacorpus = list(source = "news"))
 ctwitter <- corpus(twitter, metacorpus = list(source = "twitter"))
 rm(blogs, news, twitter)
-save(cblogs, cnews, ctwitter, file = "data/corpusQ2.Rdata")
-
-tblogs <- tokens(cblogs, what = "word", remove_separators = TRUE)
-tnews <- tokens(cnews, what = "word", remove_separators = TRUE)
-ttwitter <- tokens(ctwitter, what = "word", remove_separators = TRUE)
-save (tblogs, tnews, ttwitter, file = "data/tokensQ2.Rdata")
-
-dblogs <- dfm(tblogs)
-dnews <- dfm(tnews)
-dtwitter <- dfm(ttwitter)
+save(cblogs, cnews, ctwitter, file = "data/corpusQ.Rdata")
