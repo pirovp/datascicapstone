45ea72a437bca06d11a8157530d43c5665eb39d4
pyrop2000 <pyrop2000@gmail.com>
2019-01-29 23:38:36 +0000

    bugs in predict

--- scripts/model_class.R
+++ scripts/model_class.R
@@ -24,18 +24,23 @@ setMethod(f="predict",
                 
                 # cut input sentence to (n-1)gram
                 max_n <- min(object@max_n, length(sentence)+1)
-                ngram <- tail(sentence, max_n-1)
+                n_1gram <- tail(sentence, max_n-1)
 
                 # find all matching n-grams given (n-1)gram, for n=1 to max_n
                 possible_ngrams <- purrr::map_dfr(max_n:1, function(i) {
+                        if (i>1) cond_gram <- tail(n_1gram, i-1) else cond_gram <- "1"
                         filter(cond_probs, ngram_level==i)$cond_probs[[1]] %>%
-                        filter(conditioned_on==paste(max(tail(ngram, i-1),1), collapse = "_"))
+                        filter(conditioned_on==paste(cond_gram, collapse = "_"))
                         }
                 )
                 
                 # choose the best -depends on model
+                vNgramProbability <- Vectorize(ngramProbability, "ngram")
+                #stupid b-o should be treated specially
                 best_ngrams <- mutate(possible_ngrams,
-                                      probability=ngramProbability(ngram, possible_ngrams)) %>%
+                                      probability=vNgramProbability(strsplit(feature, "_"),
+                                                                   possible_ngrams)) %>%
                                arrange(desc(probability))
+                return(best_ngrams[1:10,])
           }
 )
