e908b850a81ac91b45a78b2c381276f5c6fa5331
pyrop2000 <pyrop2000@gmail.com>
2019-02-11 22:45:25 +0000

    streamline stupid backoff

--- scripts/M_stupid_backoff.R
+++ scripts/M_stupid_backoff.R
@@ -1,12 +1,12 @@
-stupidBackoffProb <- function(ngram, matches) {
-      n <- length(ngram)
-      # this shouldn't be necessary if coming from eval method but oh well
-      subgrams <- purrr::map_dfr(n:1,
-                                 ~filter(matches,
-                                         feature==paste(tail(ngram, .),
-                                                        collapse = "_"))
-                            )
-      
-      #this should give the stupid backoff result by itself
-      return(subgrams$condprob[1])
+# no need to do anything in eval mode
+stupidBackoff_eval <- function(ngram, matches) {
+      return(matches$condprob[1])
+}
+
+#
+stupidBackoff_predict <- function(matches) {
+      return(mutate(matches,
+                    predicted_word=sub(".*_", "", feature)
+                    )
+      )
 }
