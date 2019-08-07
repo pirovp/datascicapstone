229ace8f8c3d1905d8e717109c912ce0625d1b4c
pirovp <pyrop2000@gmail.com>
2019-08-07 16:12:14 +0100

    full feature implementation

--- shiny/ui.R
+++ shiny/ui.R
@@ -7,21 +7,45 @@ ui <- fluidPage(theme = shinythemes::shinytheme("journal"),
             tabPanel(
                   "App",
                   br(),
-                  fluidRow(column(4,
+                  fluidRow(column(8,
                         textInput("user_text",
                                   label = NULL, 
                                   value = default_str,
                                   width = "100%")
+                        )),
+                  br(),
+                  fluidRow(
+                        column(1
                         ),
                         column(1,
-                               textOutput("prediction")
+                               actionButton("prediction1", label = "")
+                        ),
+                        column(1,
+                               actionButton("prediction2", label = "")
+                        ),
+                        column(1,
+                               actionButton("prediction3", label = "")
+                        ),
+                        column(1,
+                               actionButton("prediction4", label = "")
+                        ),
+                        column(1,
+                               actionButton("prediction5", label = "")
                         )),
+                  br(),
                   fluidRow(column(8,
-                        textOutput("more_predictions")
+                                  "Click on a predicted word above, or generate a whole sentence.")),
+                  br(),
+                  fluidRow(
+                        column(1),
+                        column(6,
+                        actionButton("rs_button", label = "Predict Sentence"),
+                        selectInput("nwords", "Number of new words:", 1:30, 20)
                         )),
+                  br(),
                   fluidRow(column(8,
-                        plotOutput("probs_plot")
-                        ))
+                        textOutput("rs_text")
+                  ))
                   ),
             
             tabPanel(
